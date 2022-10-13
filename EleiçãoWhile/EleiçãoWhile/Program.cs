ceaf71b074f92f62bd8eaf82149bdd8e05a8134e
CarlosFerreiraSilva <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2022-10-13 11:30:12 -0300

    PrimeiroEnvio
    Enviar

---
+++ EleiçãoWhile/EleiçãoWhile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EleiçãoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mãe de Pedro");
+        }
+    }
+}
