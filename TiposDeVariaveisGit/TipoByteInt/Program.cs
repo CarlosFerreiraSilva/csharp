81d805301ff61f055ac5663112bac5f714da65de
CarlosFerreiraSilva <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2022-10-24 18:13:22 -0300

    Byte

---
+++ TiposDeVariaveisGit/TipoByteInt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TipoByteInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte v1 = 44;
+            Console.WriteLine("O valor de v1 é {0}",v1);
+            Console.WriteLine("O tipo de v1 é {0}", v1.GetType());
+
+            Console.WriteLine("Digite um valor na faixa de 0 a 255");
+
+            try
+            {
+                byte vdigitado = Convert.ToByte(Console.Readline());
+                Console.WriteLine("O valor digitado é {0}", vdigitado);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("O valor digitado não é byte");
+            }
+        }
+    }
+}
